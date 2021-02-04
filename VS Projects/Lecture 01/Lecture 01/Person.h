d0ac6b516ddf982b653a79c1deae6b3f1b6fb3e7
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2021-02-04 22:31:34 +0100

    Moved over to VS

---
+++ VS Projects/Lecture 01/Lecture 01/Person.h
@@ -0,0 +1,22 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+#include "Date.h"
+
+class Person {
+public:
+    Person();
+
+    std::string getName() const;
+    void setName(const std::string& name);
+
+    void reset();
+
+private:
+    std::string name;
+    Date date;
+
+};
+
+#endif // PERSON_H
