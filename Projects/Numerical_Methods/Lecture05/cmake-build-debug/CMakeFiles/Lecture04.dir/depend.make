ee8515de61a3dc215afcab09bd7c50118527e164
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2022-03-29 17:47:43 +0200

    agawldæwad

--- Projects/Numerical_Methods/Lecture05/cmake-build-debug/CMakeFiles/Lecture04.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Lecture04.
-# This may be replaced when dependencies are built.
--- Projects/Numerical_Methods/Lecture05/cmake-build-debug/cmake_install.cmake
+++ Projects/Numerical_Methods/Lecture05/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Documents/GitHub/CPP/Projects/Numerical_Methods/Lecture05
+# Install script for directory: C:/Users/Thomas Therkelsen/Documents/GitHub/CPP/Projects/Numerical_Methods/Lecture05
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Lecture04")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Lecture05")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Documents/GitHub/CPP/Projects/Numerical_Methods/Lecture05/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Thomas Therkelsen/Documents/GitHub/CPP/Projects/Numerical_Methods/Lecture05/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
