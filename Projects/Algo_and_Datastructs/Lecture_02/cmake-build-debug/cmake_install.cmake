78e5c2ddeef911e317c7aba621ecfd0cba887dc1
Therkelsen <thomastherkelsen191200@gmail.com>
2022-09-27 15:50:31 +0200

    Fixed naming

--- Projects/Algo_and_Datastructs/Lecture_02/cmake-build-debug/cmake_install.cmake
+++ Projects/Algo_and_Datastructs/Lecture_02/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/therkelsen/CPP/Projects/Algo_and_Datastructs/Lecture02
+# Install script for directory: /home/therkelsen/CPP/Projects/Algo_and_Datastructs/Lecture_02
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/therkelsen/CPP/Projects/Algo_and_Datastructs/Lecture02/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/therkelsen/CPP/Projects/Algo_and_Datastructs/Lecture_02/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Projects/Algo_and_Datastructs/Lecture_02/main.cpp
+++ Projects/Algo_and_Datastructs/Lecture_02/main.cpp
@@ -1,4 +1,4 @@
-#include "stack.h"
+#include <iostream>
 
 using namespace std;
 
